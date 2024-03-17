91605e5d2fe618d9a6430d2006936784ca5aefef
Ibrahim AlDekhail <idekhail@googlemail.com>
2024-03-17 12:52:57 +0300

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import 'package:flutter/services.dart';
@@ -11,120 +10,123 @@ class MyApp extends StatelessWidget {
   final userController = TextEditingController();
   final passController = TextEditingController();
   final showController = TextEditingController();
+
+  MyApp({super.key});
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         //============================
         appBar: AppBar(
-                  backgroundColor: Colors.green,
-                  title: Text('Login Page'),
+          backgroundColor: Colors.green,
+          title: const Text('Login Page'),
         ),
         backgroundColor: Colors.grey,
         body: Column(
           children: [
             //============================
             Container(
-              margin: EdgeInsets.all(10.0),
-              //color: Colors.white12,
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.redAccent,
               //============================
               child: TextField(
                 controller: userController,
                 keyboardType: TextInputType.name,
                 decoration: const InputDecoration(
                   filled: true,
-                  //fillColor: Color(0xffF02E65),
                   fillColor: Color.fromARGB(255, 255, 255, 255),
-                 // fillColor: Colors.deepPurpleAccent,
                   hintText: 'Username',
                   hintStyle: TextStyle(
                     color: Colors.black,
-                    fontSize: 25,
-                   // backgroundColor: Colors.white,
+                    fontSize: 20,
+                    // backgroundColor: Colors.white,
                   ),
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderRadius: BorderRadius.all(Radius.circular(30.0)),
                   ),
                 ),
               ),
               //============================
             ),
             Container(
-              margin: EdgeInsets.all(10.0),
-              color: Colors.white12,
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.redAccent,
               //============================
-              child: TextField(
+              child: TextFormField(
                 controller: passController,
-                keyboardType: TextInputType.name,
-                decoration: const InputDecoration(
-                  filled: true,
-                  fillColor: Color.fromARGB(255, 255, 255, 255),
-                  hintText: 'Password',
-                  hintStyle: TextStyle(
+                decoration:  const InputDecoration(
+                  labelText: 'Enter your password',
+                  labelStyle: TextStyle(
                     color: Colors.black,
-                    fontSize: 25,
-                    backgroundColor: Colors.white,
-                  ),
+                    fontSize: 20,
+                  ),                    filled: true,
+                  fillColor: Colors.white,
                   border: OutlineInputBorder(
-                    borderRadius: BorderRadius.all(Radius.circular(20.0)),
+                    borderRadius: BorderRadius.all(Radius.circular(30.0),),
                   ),
                 ),
               ),
             ),
             //============================
             Container(
-              margin: EdgeInsets.all(10.0),
-              height: 100, width: 400,
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(15),
               color: Colors.brown,
-              alignment: Alignment.center,
+              // alignment: Alignment.center,
               //============================
-              child: Row(
-                children: [
-                  Expanded(
-                    child: FloatingActionButton(
-                      onPressed: () {
-                        showController.text = userController.text.toString();
-                        showController.text +="  ---   ";
-                        showController.text += passController.text.toString();
-                      },
-                      child: Text("Login", style: TextStyle(fontSize: 20, color: Colors.white),),
-                      backgroundColor: Color.fromARGB(200, 000, 000, 200),
+              child:Center(
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: FloatingActionButton(
+                        onPressed: () {
+                          showController.text = userController.text.toString();
+                          showController.text +="  ---   ";
+                          showController.text += passController.text.toString();
+                        },
+                        backgroundColor: const Color.fromARGB(200, 000, 000, 200),
+                        child: const Text("Login", style: TextStyle(fontSize: 20, color: Colors.white),),
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: ElevatedButton(
-                      onPressed: () {
-                        showController.text = '';
-                        userController.text= '';
-                        passController.text= '';
-                      },
-                      child: Text("Clear", style: TextStyle(fontSize: 20, color: Colors.white),),
-                      style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                    Expanded(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          showController.text = '';
+                          userController.text= '';
+                          passController.text= '';
+                        },
+                        style: ElevatedButton.styleFrom(backgroundColor: Colors.black),
+                        child: const Text("Clear", style: TextStyle(fontSize: 20, color: Colors.white),),
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    child: CloseButton(
-                      color: Colors.white,
-                      onPressed: () {
-                        if (Platform.isAndroid) {
-                          SystemNavigator.pop();
-                          SystemChannels.platform.invokeMethod('SystemNavigator.pop');
-                        } else if (Platform.isIOS) {
-                          exit(0);
-                        }
-                      },
+                    Expanded(
+                      child: CloseButton(
+                        color: Colors.white,
+                        onPressed: () {
+                          if (Platform.isAndroid) {
+                            SystemNavigator.pop();
+                            SystemChannels.platform.invokeMethod('SystemNavigator.pop');
+                            exit(0);
+                          } else if (Platform.isIOS) {
+                            exit(0);
+                          }
+                        },
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
             Container(
-              margin: EdgeInsets.all(10.0),
+              margin: const EdgeInsets.all(10.0),
               color: Colors.white12,
+              height: 200, width: 400,
               //============================
               child: TextField(
                 controller: showController,
-               // keyboardType: TextInputType.name,
+                keyboardType: TextInputType.name,
                 decoration: const InputDecoration(
                   filled: true,
                   fillColor: Color.fromARGB(255, 255, 255, 255),
